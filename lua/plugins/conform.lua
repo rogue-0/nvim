fc4df4752c78651b01b3c6f12552bbb000a3473d
cosmo <cosmo>
2024-08-04 23:57:48 +0500

    remove outdated syntax

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,11 +6,11 @@ return {
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
-				javascript = { { "prettierd", "prettier" } },
-				javascriptreact = { { "prettierd", "prettier" } },
-				typescript = { { "prettierd", "prettier" } },
-				typescriptreact = { { "prettierd", "prettier" } },
-				vue = { { "prettierd", "prettier" } },
+				javascript = { "prettierd" },
+				javascriptreact = { "prettierd" },
+				typescript = { "prettierd" },
+				typescriptreact = { "prettierd" },
+				vue = { "prettierd" },
 			},
 			format_after_save = {
 				lsp_fallback = true,
